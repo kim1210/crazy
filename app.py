2dcaa2fb6e2609bc7a29024b35134ae82ee1953d
kim1210 <69778811+kim1210@users.noreply.github.com>
2021-04-05 22:55:43 +0900

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import pprint
 
 app = Flask(__name__)
 
@@ -8,24 +9,36 @@ def main():
 
 @app.route('/ajax_test', methods=['GET', 'POST'])
 def ajax_test():
-    if request.method =='POST':        
-        return redirect(url_for('success'))
+    # testdata = request.get_json()
+    # print(testdata)
+    if request.method =='POST': 
+        testdata = request.get_json()
+        print(testdata)
+        print(testdata[0])
+        # print('으아아아')
+        # pprint.pprint(request)
+        return redirect(url_for('success', testdata=testdata))
     return render_template('ajax_test.html')
 
-@app.route('/success', methods=['POST'])
-def success():
-    testdata = request.get_json()
-    print(testdata)
-    return render_template('success.html', testdata = testdata)
-
-# @app.route('/success', methods=['GET', 'POST'])
+# @app.route('/success', methods=['POST'])
 # def success():
-#     if request.method == 'POST':
-#         testdata = request.get_json()
-#         print(testdata)
-#         return render_template('success.html', testdata=testdata)
-#     testdata=request.get_json()
+#     testdata = request.get_json()
 #     print(testdata)
-#     return render_template('success.html', testdata=testdata)
+#     print(type(testdata))
+#     return render_template('success.html', testdata = testdata)
+
+@app.route('/success', methods=['GET', 'POST'])
+def success():
+    if request.method == 'POST':
+        testdata = request.get_json()
+        # pprint.pprint(request)
+        print('post')
+        print(testdata)
+        print(testdata[0]['text'])
+        return render_template('success.html', testdata=testdata)
+    testdata=request.get_json()
+    print(testdata)
+    print('get')
+    return render_template('success.html', testdata=testdata)
 
 app.run(debug=True)
